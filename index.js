1316cb78450e4b94421ee1632464a714a02e8d64
KenanAljaber <keno12333@hotmail.com>
2025-05-13 12:08:17 +0200

    test implementation

---
+++ index.js
@@ -0,0 +1,7 @@
+function addNumbers (num1, num2) {
+        console.log(num1, num2);
+        
+        return num1 + num2;
+    }
+    
+    module.exports = addNumbers;
---
+++ test/sumtest.test.js
@@ -0,0 +1,7 @@
+const addNumbers = require("..");
+
+describe("sum", () => {
+    test("adds 1 + 2 to equal 3", () => {
+        expect(addNumbers(1, 2)).toBe(3);
+    });
+});
