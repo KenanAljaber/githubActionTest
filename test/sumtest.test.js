f27072d30256982205b68afa869b77da7947a35a
KenanAljaber <keno12333@hotmail.com>
2025-05-14 11:03:46 +0200

    only run the cd pipeline if the first is done

--- test/sumtest.test.js
+++ test/sumtest.test.js
@@ -2,6 +2,6 @@ const addNumbers = require("..");
 
 describe("sum", () => {
     test("adds 1 + 2 to equal 3", () => {
-        expect(addNumbers(1, 2)).toBe(3);
+        expect(addNumbers(1, 2)).toBe(5);
     });
 });
